2de1ebb3f5231d8e5e60bc441e0209d1bf9d8b26
alden3721 <alden.ys.lo@gmail.com>
2024-03-06 15:18:13 -0800

    test

--- grade.sh
+++ grade.sh
@@ -14,3 +14,62 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+submission=$(find "student-submission" -type f -name "*.java")
+file_name=$(basename $submission .java)
+
+if [ -n "$file_name" ];
+then
+    echo "file found"
+    echo $file_name
+else
+    echo "File not found"
+    echo "Grade: 0%"
+    
+fi
+
+cp $submission TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+ls TestListExamples.java
+
+if [ $file_name = "ListExamples" ];
+then :
+else
+    cp TestListExamples.java TestListExamples.txt
+
+    sed -i '' -e s/ListExamples/$file_name/g TestListExamples.txt
+
+    mv TestListExamples.txt Test$file_name.java
+
+    rm TestListExamples.java
+fi
+
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]];
+then
+    echo "compilation successful"
+else
+    echo "compilation not successful"
+    echo "Grade: 0%"
+    
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore Test$file_name > test_output.txt
+
+if grep -q "OK" test_output.txt;
+then
+    echo "Grade: 100%"
+else
+    tests_run=$(grep -o "Tests run: [0-9]*" test_output.txt | cut -d' ' -f3)
+    failures=$(grep -o "Failures: [0-9]*" test_output.txt | cut -d' ' -f2)
+
+    successful_tests=$((tests_run - failures))
+    percentage=$(echo "scale=2; ($successful_tests / $tests_run) * 100" | bc)
+
+    echo "Grade: $percentage%"
+fi
